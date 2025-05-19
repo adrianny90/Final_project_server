3b469985d191cd139e34697ec9220293e0d01fa3
Adrian B <adrianny90@wp.pl>
2025-05-19 13:57:33 +0200

    add cloudinary

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import "./db/dbConnection.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import userRouter from "./routes/userRouter.js";
 import itemRouter from "./routes/itemRouter.js";
-
+import upload from "./middlewares/upload.js";
 const server = express();
 const port = process.env.PORT || 3000;
 
@@ -13,6 +13,14 @@ server.use(cors());
 
 server.use("/users", userRouter);
 server.use("/items", itemRouter);
+
+server.post("/image-upload", upload.single("img"), (req, res) => {
+  console.log(req.file);
+
+  // Example how to store the image url in your database.
+  // const product = await ProductSchema.findByIdAndUpdate({img:req.file.secure_url, imgPublicId: req.file.public_id })
+  res.json({ msg: "Image uploaded", img: req.file.secure_url });
+});
 server.use("/*splat", (req, res) =>
   res.status(404).json({ error: "Not found" })
 );
